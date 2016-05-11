51712766056eb0b7f57bbece1571fe42d4395ece
Edgar Solomonik <solomonik@inf.ethz.ch>
2016-05-11 13:31:53 +0200

    corrected sym definition and iterator bugs in bench_ctf.cpp, filled U with random values, added error check with respect to manual factorization

--- benchmarks/bench_ctf/bench_ctf.cpp
+++ benchmarks/bench_ctf/bench_ctf.cpp
@@ -28,13 +28,13 @@ int main(int argc, char ** argv) {
   int prop_shapes[6] = { NS, NS, NS, NS, NS, NS };
  
   int tau_sizes[2] = {Fs, Fs};
-  int tau_shapes[2] = {Fs, Fs};
+  int tau_shapes[2] = {NS, NS};
   
   int gamma_sizes[2] = {Ds, Ds};
-  int gamma_shapes[2] = {Ds, Ds};
+  int gamma_shapes[2] = {NS, NS};
 
   int su3_sizes[2] = {Cs, Cs};
-  int su3_shapes[2] = {Cs, Cs};
+  int su3_shapes[2] = {NS, NS};
 
   Tensor< std::complex<double> > S1(6, prop_sizes, prop_shapes, dw);
   Tensor< std::complex<double> > S2(6, prop_sizes, prop_shapes, dw);
@@ -47,6 +47,8 @@ int main(int argc, char ** argv) {
   Tensor< std::complex<double> > g0(2, gamma_sizes, gamma_shapes, dw);
 
   Tensor< std::complex<double> > U(2, su3_sizes, su3_shapes, dw);
+  srand48(rank);
+  ((Transform< std::complex<double> >)([](std::complex<double> & u){ u= std::complex<double>(drand48(),drand48()); }))(U["ij"]);
 
   rlxd_init(1,123213);
   double* r = (double*)malloc(2*2*sizeof(double)*Ds*Ds*Cs*Cs*Fs*Fs);
@@ -81,7 +83,7 @@ int main(int argc, char ** argv) {
   r = (double*)malloc(2*sizeof(double)*Fs*Fs);
   ranlxd(r, 2*Fs*Fs);
   j=0;
-  for(int i=0; i<npair; ++j){
+  for(int i=0; i<npair; ++i){
     pairs[i] = std::complex<double>(r[j],r[j+1]);
     j+=2;
   }
@@ -119,7 +121,24 @@ int main(int argc, char ** argv) {
   // Segmentation fult here...  
   Cc["xy"] = S1["abcdxf"]*Phi["bg"]*tau3["gh"]*g0["di"]*S2["jhkilf"]*Phi["jm"]*tau3["ma"]*g0["kc"]*U["ly"];
   
-  Cc.print();
+  Tensor< std::complex<double> > Cc2(2,su3_sizes,su3_shapes,dw);
+  S1["agcdxf"] = S1["abcdxf"]*Phi["bg"];
+  S1["ahcdxf"] = S1["agcdxf"]*tau3["gh"];
+  S1["ahcixf"] = S1["ahcdxf"]*g0["di"];
+  S2["mhkilf"] = S2["jhkilf"]*Phi["jm"];
+  S2["ahkilf"] = S2["mhkilf"]*tau3["ma"];
+  S2["ahcilf"] = S2["ahkilf"]*g0["kc"];
+  S2["ahciyf"] = S2["ahcilf"]*U["ly"];
+  Cc2["xy"] = S1["ahcixf"]*S2["ahciyf"];
+  //Cc.print();
+  //Cc2.print();
+  Cc2["xy"] -= Cc["xy"];
+  Tensor<double> err(2,su3_sizes,su3_shapes,dw);
+  err["xy"] = ((Function< std::complex<double>, double >)([](std::complex<double> c){ return c.real()*c.real() + c.imag()*c.imag(); }))(Cc2["xy"]);
+  double nrm_err_Cc2 = err["xy"];
+  nrm_err_Cc2 = sqrt(nrm_err_Cc2);
+  if (rank == 0) printf("error norm = %E\n", nrm_err_Cc2);
+
 
   return 0;
 }
