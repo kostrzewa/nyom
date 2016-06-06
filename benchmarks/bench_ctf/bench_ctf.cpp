647d1c257b2a9ede76430fbb153172a9bd1fffce
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-06-06 14:30:30 +0200

    benchmark_ctf: some further additions for the fully local contraction (without shifts)

--- benchmarks/bench_ctf/bench_ctf.cpp
+++ benchmarks/bench_ctf/bench_ctf.cpp
@@ -12,6 +12,7 @@
 #define Cs 3
 #define Fs 2
 #define Ss 4
+#define Xs 256
 
 using namespace CTF;
 using namespace std;
@@ -30,29 +31,39 @@ int main(int argc, char ** argv) {
   int tau_sizes[2] = {Fs, Fs};
   int tau_shapes[2] = {NS, NS};
   
+  int phi_sizes[2] = { Fs, Fs};
+  int phi_shapes[2] = { NS, NS};
+  
   int gamma_sizes[2] = {Ds, Ds};
   int gamma_shapes[2] = {NS, NS};
 
-  int su3_sizes[2] = {Cs, Cs};
-  int su3_shapes[2] = {NS, NS};
+  int su3_sizes[2] = { Cs, Cs};
+  int su3_shapes[2] = { NS, NS};
 
-  Tensor< std::complex<double> > S1(6, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S2(6, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S3(6, prop_sizes, prop_shapes, dw);
+ 
+  std::vector< Tensor< std::complex<double> > > S1v( Xs, Tensor< std::complex<double> >(6, prop_sizes, prop_shapes, dw) );
+  std::vector< Tensor< std::complex<double> > > S2v( Xs, Tensor< std::complex<double> >(6, prop_sizes, prop_shapes, dw) );
+  //Tensor< std::complex<double> > S2(6, prop_sizes, prop_shapes, dw);
   
-  Tensor< std::complex<double> > tau3(2, tau_sizes, tau_shapes, dw);
-  
-  Tensor< std::complex<double> > Phi(2, tau_sizes, tau_shapes, dw);
+  std::vector< Tensor< std::complex<double> > > Phiv( Xs, Tensor< std::complex<double> >(2, tau_sizes, tau_shapes, dw) );
+  std::vector< Tensor< std::complex<double> > > Uv( Xs, Tensor< std::complex<double> >(2, su3_sizes, su3_shapes, dw) );
+
+  //Tensor< std::complex<double> > Phi(2, tau_sizes, tau_shapes, dw);
+  //Tensor< std::complex<double> > U(2, su3_sizes, su3_shapes, dw);
 
   Tensor< std::complex<double> > g0(2, gamma_sizes, gamma_shapes, dw);
+  Tensor< std::complex<double> > tau3(2, tau_sizes, tau_shapes, dw);
 
-  Tensor< std::complex<double> > U(2, su3_sizes, su3_shapes, dw);
+  if(rank==0) std::cout << S1v.size() << std::endl;
+
   srand48(rank);
-  ((Transform< std::complex<double> >)([](std::complex<double> & u){ u= std::complex<double>(drand48(),drand48()); }))(U["ij"]);
+  for(auto &&U : Uv){
+    ((Transform< std::complex<double> >)([](std::complex<double> & u){ u= std::complex<double>(drand48(),drand48()); }))(U["ij"]);
+  }
 
   rlxd_init(1,123213);
-  double* r = (double*)malloc(2*2*sizeof(double)*Ds*Ds*Cs*Cs*Fs*Fs);
-  ranlxd(r, 2*2*Ds*Ds*Cs*Cs*Fs*Fs);
+  double* r = (double*)malloc(2*2*sizeof(double)*Xs*Ds*Ds*Cs*Cs*Fs*Fs);
+  ranlxd(r, 2*2*Xs*Ds*Ds*Cs*Cs*Fs*Fs);
 
   unsigned long int j = 0;
   std::complex<double>* pairs;
@@ -60,35 +71,45 @@ int main(int argc, char ** argv) {
   int64_t  npair;
 
   // fill "propagators" with random values
-  S1.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    pairs[i] = std::complex<double>(r[j],r[j+1]);
-    j+=2;
+  for(auto &&S1 : S1v){
+    S1.read_local(&npair, &indices, &pairs);
+    for(int i=0; i<npair; ++i){
+      pairs[i] = std::complex<double>(r[j],r[j+1]);
+      j+=2;
+    }
+    S1.write(npair, indices, pairs);
+    free(pairs); free(indices);
   }
-  S1.write(npair, indices, pairs);
-  free(pairs); free(indices);
-  
-  S2.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    pairs[i] = std::complex<double>(r[j],r[j+1]);
-    j+=2;
+ 
+  for(auto &&S2 : S2v){ 
+    S2.read_local(&npair, &indices, &pairs);
+    for(int i=0; i<npair; ++i){
+      pairs[i] = std::complex<double>(r[j],r[j+1]);
+      j+=2;
+    }
+    S2.write(npair, indices, pairs);
+    free(pairs); free(indices);
   }
-  S2.write(npair, indices, pairs);
-  free(pairs); free(indices);
   
   free(r);
   //
   // as well as the scalar field
-  Phi.read_local(&npair, &indices, &pairs);
-  r = (double*)malloc(2*sizeof(double)*Fs*Fs);
-  ranlxd(r, 2*Fs*Fs);
+  r = (double*)malloc(2*sizeof(double)*Xs*Fs*Fs);
+  ranlxd(r, 2*Xs*Fs*Fs);
+  
+  r = (double*)malloc(2*sizeof(double)*Xs*Fs*Fs);
+  ranlxd(r, 2*Xs*Fs*Fs);
   j=0;
-  for(int i=0; i<npair; ++i){
-    pairs[i] = std::complex<double>(r[j],r[j+1]);
-    j+=2;
+  
+  for(auto &&Phi : Phiv){ 
+    Phi.read_local(&npair, &indices, &pairs);
+    for(int i=0; i<npair; ++i){
+      pairs[i] = std::complex<double>(r[j],r[j+1]);
+      j+=2;
+    }
+    Phi.write(npair, indices, pairs);
+    free(pairs); free(indices);
   }
-  Phi.write(npair, indices, pairs);
-  free(pairs); free(indices);
   free(r);
   
   // now construct gamma^0
@@ -118,26 +139,42 @@ int main(int argc, char ** argv) {
   
   Tensor< std::complex<double> > Cc(2,su3_sizes,su3_shapes,dw);
   
-  // Segmentation fult here...  
-  Cc["xy"] = S1["abcdxf"]*Phi["bg"]*tau3["gh"]*g0["di"]*S2["jhkilf"]*Phi["jm"]*tau3["ma"]*g0["kc"]*U["ly"];
+  // this is very slow because of temporaries
+  //Cc["xy"] = S1["abcdxf"]*Phi["bg"]*tau3["gh"]*g0["di"]*S2["jhkilf"]*Phi["jm"]*tau3["ma"]*g0["kc"]*U["ly"];
   
   Tensor< std::complex<double> > Cc2(2,su3_sizes,su3_shapes,dw);
-  S1["agcdxf"] = S1["abcdxf"]*Phi["bg"];
-  S1["ahcdxf"] = S1["agcdxf"]*tau3["gh"];
-  S1["ahcixf"] = S1["ahcdxf"]*g0["di"];
-  S2["mhkilf"] = S2["jhkilf"]*Phi["jm"];
-  S2["ahkilf"] = S2["mhkilf"]*tau3["ma"];
-  S2["ahcilf"] = S2["ahkilf"]*g0["kc"];
-  S2["ahciyf"] = S2["ahcilf"]*U["ly"];
-  Cc2["xy"] = S1["ahcixf"]*S2["ahciyf"];
+  S1v[0]["agcdxf"] = S1v[0]["abcdxf"]*Phiv[0]["bg"];
+  S1v[0]["ahcdxf"] = S1v[0]["agcdxf"]*tau3["gh"];
+  S1v[0]["ahcixf"] = S1v[0]["ahcdxf"]*g0["di"];
+  S2v[0]["mhkilf"] = S2v[0]["jhkilf"]*Phiv[0]["jm"];
+  S2v[0]["ahkilf"] = S2v[0]["mhkilf"]*tau3["ma"];
+  S2v[0]["ahcilf"] = S2v[0]["ahkilf"]*g0["kc"];
+  S2v[0]["ahciyf"] = S2v[0]["ahcilf"]*Uv[0]["ly"];
+  Cc2["xy"] = S1v[0]["ahcixf"]*S2v[0]["ahciyf"];
+
+  for(size_t x = 1; x < Xs; ++x){
+    S1v[x]["agcdxf"] = S1v[x]["abcdxf"]*Phiv[0]["bg"];
+    S1v[x]["ahcdxf"] = S1v[x]["agcdxf"]*tau3["gh"];
+    S1v[x]["ahcixf"] = S1v[x]["ahcdxf"]*g0["di"];
+    S2v[x]["mhkilf"] = S2v[x]["jhkilf"]*Phiv[0]["jm"];
+    S2v[x]["ahkilf"] = S2v[x]["mhkilf"]*tau3["ma"];
+    S2v[x]["ahcilf"] = S2v[x]["ahkilf"]*g0["kc"];
+    S2v[x]["ahciyf"] = S2v[x]["ahcilf"]*Uv[0]["ly"];
+    Cc2["xy"] += S1v[x]["ahcixf"]*S2v[x]["ahciyf"];
+  }
+
+  Cc2.print();
+
+  //Cc2.print();
+
   //Cc.print();
   //Cc2.print();
-  Cc2["xy"] -= Cc["xy"];
-  Tensor<double> err(2,su3_sizes,su3_shapes,dw);
-  err["xy"] = ((Function< std::complex<double>, double >)([](std::complex<double> c){ return c.real()*c.real() + c.imag()*c.imag(); }))(Cc2["xy"]);
-  double nrm_err_Cc2 = err["xy"];
-  nrm_err_Cc2 = sqrt(nrm_err_Cc2);
-  if (rank == 0) printf("error norm = %E\n", nrm_err_Cc2);
+  //Cc2["xy"] -= Cc["xy"];
+  //Tensor<double> err(2,su3_sizes,su3_shapes,dw);
+  //err["xy"] = ((Function< std::complex<double>, double >)([](std::complex<double> c){ return c.real()*c.real() + c.imag()*c.imag(); }))(Cc2["xy"]);
+  //double nrm_err_Cc2 = err["xy"];
+  //nrm_err_Cc2 = sqrt(nrm_err_Cc2);
+  //if (rank == 0) printf("error norm = %E\n", nrm_err_Cc2);
 
 
   return 0;
