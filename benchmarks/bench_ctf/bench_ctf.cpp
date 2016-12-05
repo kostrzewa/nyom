63b7f6d008ac4788e6b6f870055dcce037002a3a
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-12-05 15:33:50 +0100

    bench_ctf: add time measurements for individual components

--- benchmarks/bench_ctf/bench_ctf.cpp
+++ benchmarks/bench_ctf/bench_ctf.cpp
@@ -13,12 +13,31 @@
 #define Cs 3
 #define Fs 2
 
-#define Ts 16
-#define Ls 8
+#define Ts 24
+#define Ls 12
 
 using namespace CTF;
 using namespace std;
 
+void timeDiff(
+        const std::chrono::time_point<std::chrono::steady_clock>& time, 
+        const char* const name,
+        const int rank){
+  std::chrono::duration<float> elapsed_seconds = std::chrono::steady_clock::now() - time;
+  if(rank==0){ 
+    cout << "Time for " << name << " " << elapsed_seconds.count() <<
+      " seconds" << std::endl;
+  }
+}
+
+void timeDiffAndUpdate(
+        std::chrono::time_point<std::chrono::steady_clock>& time, 
+        const char* const name,
+        const int rank){
+  timeDiff(time,name,rank);
+  time = std::chrono::steady_clock::now();
+}
+  
 int main(int argc, char ** argv) {
   int rank, np, d;
   MPI_Init(&argc,&argv);
@@ -54,6 +73,8 @@ int main(int argc, char ** argv) {
   
   Tensor< std::complex<double> > S1_m1(10, prop_sizes, prop_shapes, dw);
   Tensor< std::complex<double> > S2_m1(10, prop_sizes, prop_shapes, dw);
+  Tensor< std::complex<double> > S1_m2(10, prop_sizes, prop_shapes, dw);
+  Tensor< std::complex<double> > S2_m2(10, prop_sizes, prop_shapes, dw);
  
   Tensor< std::complex<double> > S_t1(10, prop_sizes, prop_shapes, dw);
   Tensor< std::complex<double> > S_t2(10, prop_sizes, prop_shapes, dw);
@@ -61,9 +82,6 @@ int main(int argc, char ** argv) {
   Tensor< std::complex<double> > S_f1(10, prop_sizes, prop_shapes, dw);
   Tensor< std::complex<double> > S_f2(10, prop_sizes, prop_shapes, dw);
 
-  Tensor< std::complex<double> > S1_m2(10, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S2_m2(10, prop_sizes, prop_shapes, dw);
-  
   Tensor< std::complex<double> > Phi(6, phi_sizes, phi_shapes, dw);
   Tensor< std::complex<double> > Phi_m1(6, phi_sizes, phi_shapes, dw);
   Tensor< std::complex<double> > Phi_m2(6, phi_sizes, phi_shapes, dw);
@@ -174,43 +192,61 @@ int main(int argc, char ** argv) {
   tau3.write(npair, indices, pairs);
   free(pairs); free(indices);
  
+  std::chrono::time_point<std::chrono::steady_clock> totalstart = std::chrono::steady_clock::now();
   std::chrono::time_point<std::chrono::steady_clock> start;
-  std::chrono::duration<float> elapsed_seconds;
 
   start = std::chrono::steady_clock::now();
-  
   // construct time shifted fields
   U_m1["txyzab"] = Tshift_m1["tT"]*U["Txyzab"];
+  timeDiffAndUpdate(start,"T-shift 1 GF",rank);
   U_m2["txyzab"] = Tshift_m2["tT"]*U["Txyzab"];
+  timeDiffAndUpdate(start,"T-shift 2 GF",rank);
 
   Phi_m1["txyzfg"] = Tshift_m1["tT"]*Phi["Txyzfg"];
+  timeDiffAndUpdate(start,"T-shift 1 Phi",rank);
   Phi_m2["txyzfg"] = Tshift_m2["tT"]*Phi["Txyzfg"];
+  timeDiffAndUpdate(start,"T-shift 2 Phi",rank);
 
   S1_m1["txyzfgjkab"] = Tshift_m1["tT"]*S1["Txyzfgjkab"];
+  timeDiffAndUpdate(start,"T-shift 1 S1",rank);
   S2_m1["txyzfgjkab"] = Tshift_m1["tT"]*S2["Txyzfgjkab"];
+  timeDiffAndUpdate(start,"T-shift 1 S2",rank);
 
   S1_m2["txyzfgjkab"] = Tshift_m2["tT"]*S1["Txyzfgjkab"];
+  timeDiffAndUpdate(start,"T-shift 2 S1",rank);
   S2_m2["txyzfgjkab"] = Tshift_m2["tT"]*S2["Txyzfgjkab"];
+  timeDiffAndUpdate(start,"T-shift 2 S2",rank);
 
   U_t1["txyzab"] = U_m2["txyzaC"]*U_m1["txyzCb"];
+  timeDiffAndUpdate(start,"su3Xsu3",rank);
   Phi_t1["txyzfg"] = 0.5*Phi_m1["txyzfX"]*tau3["Xg"];
+  timeDiffAndUpdate(start,"phiXtau3",rank);
 
   S_t1["txyzfgijab"] = 0.5*S2_m2["txyzfgXjab"]*g0["Xi"];
+  timeDiffAndUpdate(start,"sXgamma",rank);
 
   S_t2["txyzfgijab"] = S_t1["txyzfgijaX"]*U_t1["txyzXb"];
+  timeDiffAndUpdate(start,"sXsu3",rank);
   S_f1["txyzfgijab"] = S_t2["txyzXgijab"]*Phi_t1["txyzXf"];
+  timeDiffAndUpdate(start,"sXphi",rank);
   
   Phi_t2["txyzfg"] = 0.5*Phi["txyzfX"]*tau3["Xg"];
   S_t1["txyzfgijab"] = S1["txyzfgiXab"]*g0["Xj"];
   S_f2["txyzfgijab"] = S_t1["txyzfXijab"]*Phi_t2["txyzXg"];
 
+  // compute a derivative for testing purposes
+  start = std::chrono::steady_clock::now();
+  S_t1["txyzfgijab"] = 0.5*U_m2["txyzaC"]*S1_m2["txyzfgijCb"];
+  S_t1["txyzfgijab"] -= 0.5*U_m1["txyzaC"]*S1_m1["txyzfgijCb"];
+  timeDiff(start,"derivative",rank); 
+
   Vector< std::complex<double> > C(Ts,dw);
-
+  
+  start = std::chrono::steady_clock::now();
   C["t"] = S_f1["tXYZFGIJAB"]*S_f2["tXYZFGIJAB"];
+  timeDiffAndUpdate(start,"contraction",rank);
 
-  elapsed_seconds = std::chrono::steady_clock::now()-start;
-
-  cout << "Contraction took " << elapsed_seconds.count() << " seconds" << std::endl;
+  timeDiff(totalstart,"everything",rank);
 
   C.print();
 
