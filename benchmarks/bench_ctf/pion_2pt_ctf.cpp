9ce42d79736bfdde8e363a6ca113535188035b5f
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-12-06 14:34:28 +0100

    bench_ctf: first implementation of the on the fly pion 2-pt function calculation

--- benchmarks/bench_ctf/pion_2pt_ctf.cpp
+++ benchmarks/bench_ctf/pion_2pt_ctf.cpp
@@ -15,208 +15,149 @@
 #define Cs 3
 #define Fs 2
 
-#define Ts 16
-#define Ls 8
-
 using namespace CTF;
 using namespace std;
+
+void timeDiff(
+        const std::chrono::time_point<std::chrono::steady_clock>& time, 
+        const char* const name,
+        const int rank){
+  std::chrono::duration<float> elapsed_seconds = std::chrono::steady_clock::now() - time;
+  if(rank==0){ 
+    cout << "Time for " << name << " " << elapsed_seconds.count() <<
+      " seconds" << std::endl;
+  }
+}
+
+void timeDiffAndUpdate(
+        std::chrono::time_point<std::chrono::steady_clock>& time, 
+        const char* const name,
+        const int rank){
+  timeDiff(time,name,rank);
+  time = std::chrono::steady_clock::now();
+}
 
 int main(int argc, char ** argv) {
   int rank, np, d;
 
   MPI_Init(&argc,&argv);
-  tmLQCD_invert_init(argc,argv,1,0);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
+  
+  tmLQCD_invert_init(argc,argv,1,0);
+  
+  tmLQCD_lat_params lat;
+  tmLQCD_mpi_params mpi;
+
+  tmLQCD_get_lat_params(&lat);
+  tmLQCD_get_mpi_params(&mpi);
+
+  if(rank==0){ 
+    printf("tmLQCD local lattice volume T:%02d x X:%02d x Y:%02d x Z:%02d\n", 
+              lat.T, lat.LX, lat.LY, lat.LZ);
+    printf("tmLQCD mpi params    nproc: %05d nproc_t: %02d nproc_x: %02d nproc_y: %02d nproc_z: %02d\n", 
+              mpi.nproc, mpi.nproc_t, mpi.nproc_x, mpi.nproc_y, mpi.nproc_z);
+  }
+
+  // print tmLQCD process information in order
+  for(int r = 0; r < mpi.nproc; ++r){
+      if(r == rank){ 
+        printf("tmLQCD mpi params  cart_id: %05d proc_id: %05d time_rank: %02d\n", 
+                  mpi.cart_id, mpi.proc_id, mpi.time_rank);
+        printf("tmLQCD mpi params  proc[t]: %02d proc[x]: %02d proc[y]: %02d proc_z: %02d\n", 
+                  mpi.proc_coords[0], mpi.proc_coords[1], mpi.proc_coords[2], mpi.proc_coords[3]);
+        MPI_Barrier(MPI_COMM_WORLD);
+      }else{
+        MPI_Barrier(MPI_COMM_WORLD);
+      }
+  }
 
   World dw(argc, argv);
 
-  int prop_sizes[10] = { Ts, Ls, Ls, Ls, Fs, Fs, Ds, Ds, Cs, Cs };
-  int prop_shapes[10] = { NS, NS, NS, NS, NS, NS, NS, NS, NS, NS };
+  int Ls = mpi.nproc_x * LX;
+  int Ts = mpi.nproc_t * T;
+
+  int prop_sizes[8] = { Ts, Ls, Ls, Ls, Ds, Ds, Cs, Cs };
+  int prop_shapes[8] = { NS, NS, NS, NS, NS, NS, NS, NS };
  
-  int tau_sizes[2] = {Fs, Fs};
-  int tau_shapes[2] = {NS, NS};
-  
-  int phi_sizes[6] = { Ts, Ls, Ls, Ls, Fs, Fs};
-  int phi_shapes[6] = { NS, NS, NS, NS, NS, NS};
-  
-  int gamma_sizes[2] = {Ds, Ds};
-  int gamma_shapes[2] = {NS, NS};
-  
-  // we only need the gauge links in time
-  int su3_sizes[6] = { Ts, Ls, Ls, Ls, Cs, Cs };
-  int su3_shapes[6] = { NS, NS, NS, NS, NS, NS };
-
-  int corr_sizes[1] = { Ts };
-  int corr_shapes[1] = { NS };
-
-  int Tshift_sizes[2] = {Ts, Ts};
-  int Tshift_shapes[2] = {NS, NS};
- 
-  Tensor< std::complex<double> > S1(10, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S2(10, prop_sizes, prop_shapes, dw);
-  
-  Tensor< std::complex<double> > S1_m1(10, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S2_m1(10, prop_sizes, prop_shapes, dw);
- 
-  Tensor< std::complex<double> > S_t1(10, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S_t2(10, prop_sizes, prop_shapes, dw);
-
-  Tensor< std::complex<double> > S_f1(10, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S_f2(10, prop_sizes, prop_shapes, dw);
-
-  Tensor< std::complex<double> > S1_m2(10, prop_sizes, prop_shapes, dw);
-  Tensor< std::complex<double> > S2_m2(10, prop_sizes, prop_shapes, dw);
-  
-  Tensor< std::complex<double> > Phi(6, phi_sizes, phi_shapes, dw);
-  Tensor< std::complex<double> > Phi_m1(6, phi_sizes, phi_shapes, dw);
-  Tensor< std::complex<double> > Phi_m2(6, phi_sizes, phi_shapes, dw);
-  
-  Tensor< std::complex<double> > Phi_t1(6, phi_sizes, phi_shapes, dw);
-  Tensor< std::complex<double> > Phi_t2(6, phi_sizes, phi_shapes, dw);
-  
-  Tensor< std::complex<double> > U( 6, su3_sizes, su3_shapes, dw);
-  Tensor< std::complex<double> > U_t1( 6, su3_sizes, su3_shapes, dw);
-  Tensor< std::complex<double> > U_t2( 6, su3_sizes, su3_shapes, dw);
-  Tensor< std::complex<double> > U_m1( 6, su3_sizes, su3_shapes, dw);
-  Tensor< std::complex<double> > U_m2( 6, su3_sizes, su3_shapes, dw);
-
-  Tensor< std::complex<double> > g0(2, gamma_sizes, gamma_shapes, dw);
-  Tensor< std::complex<double> > tau3(2, tau_sizes, tau_shapes, dw);
-
-  Tensor< std::complex<double> > Tshift_m1(2, Tshift_sizes, Tshift_shapes, dw);
-  Tensor< std::complex<double> > Tshift_m2(2, Tshift_sizes, Tshift_shapes, dw);
-
-  // fill tensors with random data and set up the various matrices
-  srand48(rank);
+  Tensor< std::complex<double> > S(8, prop_sizes, prop_shapes, dw);
+  Tensor< std::complex<double> > Sconj(8, prop_sizes, prop_shapes, dw);
 
   std::complex<double>* pairs;
   int64_t *indices;
   int64_t  npair;
 
-  U.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    pairs[i]=std::complex<double>(drand48(),drand48());
-  }
-  U.write(npair, indices, pairs);
-  free(pairs); free(indices);
-  
-  S1.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    pairs[i]=std::complex<double>(drand48(),drand48());
-  }
-  S1.write(npair, indices, pairs);
-  free(pairs); free(indices);
-
-  S2.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    pairs[i]=std::complex<double>(drand48(),drand48());
-  }
-  S2.write(npair, indices, pairs);
-  free(pairs); free(indices);
-
-  Phi.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    pairs[i]=std::complex<double>(drand48(),drand48());
-  }
-  Phi.write(npair, indices, pairs);
-  free(pairs); free(indices);
-
-  // construct time-shift marix (minus one unit)
-  Tshift_m1.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    // here, the "row" index runs fastest
-    int r = indices[i] % Ts;
-    int c = indices[i] / Ts;
-
-    if( c == ((r-1)+Ts) % Ts ){
-      pairs[i] = 1.0;
-    } else {
-      pairs[i] = 0.0;
-    }
-  }
-  Tshift_m1.write(npair, indices, pairs);
-  free(pairs); free(indices);
-
-  // construct time-shift matrix (minus two units)
-  Tshift_m2.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    int r = indices[i] % Ts;
-    int c = indices[i] / Ts;
-    if( c == ((r-2)+Ts) % Ts ){
-      pairs[i] = 1;
-    } else {
-      pairs[i] = 0;
-    }
-  }
-  Tshift_m2.write(npair, indices, pairs);
-  free(pairs); free(indices);
-  
-  // now construct gamma^0
-  g0.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    if( indices[i] == 2 || indices[i] == 7 || 
-        indices[i] == 8 || indices[i] == 13 ){
-      pairs[i] = 1.0;
-    } else {
-      pairs[i] = 0.0;
-    }
-  }
-  g0.write(npair, indices, pairs);
-  free(pairs); free(indices);
-  // and tau^3
-  tau3.read_local(&npair, &indices, &pairs);
-  for(int i=0; i<npair; ++i){
-    if( indices[i] == 0 ){
-      pairs[i] = 1.0;
-    } else if ( indices[i] == 3){
-      pairs[i] = -1.0;
-    } else {
-      pairs[i] = 0.0;
-    }
-  }
-  tau3.write(npair, indices, pairs);
-  free(pairs); free(indices);
- 
   std::chrono::time_point<std::chrono::steady_clock> start;
+  std::chrono::time_point<std::chrono::steady_clock> moment;
   std::chrono::duration<float> elapsed_seconds;
 
   start = std::chrono::steady_clock::now();
+
+  spinor ** temp_field = NULL;
+  init_solver_field(&temp_field,VOLUMEPLUSRAND,2);
+  tmLQCD_read_gauge(100);
+  indices = (int64_t*) calloc(4*3*VOLUME, sizeof(int64_t));
+  pairs = (std::complex<double>*) calloc(4*3*VOLUME, sizeof(std::complex<double>));
+  for(int src_s = 0; src_s < 4; ++src_s){
+    for(int src_c = 0; src_c < 3; ++src_c){ 
+      source_spinor_field(g_spinor_field[0],g_spinor_field[1], src_s, src_c);
+      convert_eo_to_lexic(temp_field[1],g_spinor_field[0],g_spinor_field[1]);
+      moment = std::chrono::steady_clock::now();
+      tmLQCD_invert((double*)temp_field[0],(double*)temp_field[1],0,0);
+      timeDiffAndUpdate(moment,"inversion",rank);
+      // globally translate from tmLQCD to Cyclops
+      int counter = 0;
+      for(int t = 0; t < T; ++t){
+        int gt = T*mpi.proc_coords[0] + t;
+        for(int x = 0; x < LX; ++x){
+          int gx = LX*mpi.proc_coords[1] + x;
+          for(int y = 0; y < LY; ++y){
+            int gy = LY*mpi.proc_coords[2] + y;
+            for(int z = 0; z < LZ; ++z){
+              int gz = LZ*mpi.proc_coords[3] + z;
+              for(int prop_s = 0; prop_s < Ds; ++prop_s){
+                for(int prop_c = 0; prop_c < Cs; ++prop_c){
+                  // global Cyclops index for S
+                  indices[counter] = src_c  * (Ts*Ls*Ls*Ls*Ds*Ds*Cs) +
+                                     prop_c * (Ts*Ls*Ls*Ls*Ds*Ds)    +
+                                     src_s  * (Ts*Ls*Ls*Ls*Ds)       +
+                                     prop_s * (Ts*Ls*Ls*Ls)          +
+                                     gz     * (Ts*Ls*Ls)             +
+                                     gy     * (Ts*Ls)                +
+                                     gx     * (Ts)                   +
+                                     gt;
+
+                  pairs[counter] = *((_Complex double*)(temp_field[0] + g_ipt[t][x][y][z] + prop_s*Cs + prop_c));
+                  counter++;
+                }
+              }
+            }
+          }
+        }
+      }
+      // global write with automatic MPI communication
+      S.write(4*3*VOLUME,indices,pairs);
+      timeDiffAndUpdate(moment,"copying propagator to tensor",rank);
+    }
+  }
+  free(indices); free(pairs);
+  finalize_solver(temp_field,2);
+ 
+  // take complex conjugate of S
+  Sconj["txyzijab"] = S["txyzijab"];
+  ((Transform< std::complex<double> >)([](std::complex<double> & s){ s = conj(s); }))(Sconj["txyzijab"]);
   
-  // construct time shifted fields
-  U_m1["txyzab"] = Tshift_m1["tT"]*U["Txyzab"];
-  U_m2["txyzab"] = Tshift_m2["tT"]*U["Txyzab"];
-
-  Phi_m1["txyzfg"] = Tshift_m1["tT"]*Phi["Txyzfg"];
-  Phi_m2["txyzfg"] = Tshift_m2["tT"]*Phi["Txyzfg"];
-
-  S1_m1["txyzfgjkab"] = Tshift_m1["tT"]*S1["Txyzfgjkab"];
-  S2_m1["txyzfgjkab"] = Tshift_m1["tT"]*S2["Txyzfgjkab"];
-
-  S1_m2["txyzfgjkab"] = Tshift_m2["tT"]*S1["Txyzfgjkab"];
-  S2_m2["txyzfgjkab"] = Tshift_m2["tT"]*S2["Txyzfgjkab"];
-
-  U_t1["txyzab"] = U_m2["txyzaC"]*U_m1["txyzCb"];
-  Phi_t1["txyzfg"] = 0.5*Phi_m1["txyzfX"]*tau3["Xg"];
-
-  S_t1["txyzfgijab"] = 0.5*S2_m2["txyzfgXjab"]*g0["Xi"];
-
-  S_t2["txyzfgijab"] = S_t1["txyzfgijaX"]*U_t1["txyzXb"];
-  S_f1["txyzfgijab"] = S_t2["txyzXgijab"]*Phi_t1["txyzXf"];
-  
-  Phi_t2["txyzfg"] = 0.5*Phi["txyzfX"]*tau3["Xg"];
-  S_t1["txyzfgijab"] = S1["txyzfgiXab"]*g0["Xj"];
-  S_f2["txyzfgijab"] = S_t1["txyzfXijab"]*Phi_t2["txyzXg"];
-
+  // perform contraction
   Vector< std::complex<double> > C(Ts,dw);
-
-  C["t"] = S_f1["tXYZFGIJAB"]*S_f2["tXYZFGIJAB"];
+  C["t"] = Sconj["tXYZIJAB"]*S["tXYZIJAB"];
 
   elapsed_seconds = std::chrono::steady_clock::now()-start;
-
-  cout << "Contraction took " << elapsed_seconds.count() << " seconds" << std::endl;
+  
+  if(rank==0)
+    cout << "Pion 2-pt function took " << elapsed_seconds.count() << " seconds" << std::endl;
 
   C.print();
-
+  
+  MPI_Finalize();
   return 0;
 }
